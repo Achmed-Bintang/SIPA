7625b6ae50166206f74e81c83beba9d8c1c430c8
.bin <145203199+Achmed-Bintang@users.noreply.github.com>
2026-05-25 12:00:25 +0700

    Update layout.tsx

--- SIPA/app/layout.tsx
+++ SIPA/app/layout.tsx
@@ -9,23 +9,8 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'SIPA - Sistem Informasi Portal Akademik Terpadu',
   description: 'Platform manajemen sekolah yang mengubah beban administratif menjadi efisiensi digital untuk kepala sekolah dan guru',
-  generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/favicon.ico',
   },
 }
 
